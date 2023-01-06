cc81fd7fec5663a359f4363b7dbeb58e47fa8bd3
Rainer Kildal <rainer.kildal@opscomsystems.com>
2023-01-06 13:15:48 +0100

    Add width and telescope harpoon marks

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,8 +1,12 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
+require("telescope").load_extension('harpoon')
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader><C-E>", function()
+	vim.cmd('Telescope harpoon marks')
+end)
 vim.keymap.set("n", "<leader>da", mark.rm_file)
 vim.keymap.set("n", "<leader>ca", mark.clear_all)
 
@@ -11,3 +15,8 @@ vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
 
+require("harpoon").setup({
+	menu = {
+		width = math.floor(vim.api.nvim_win_get_width(0) / 1.25),
+	}
+})
