d5f1a6ea470aa3ce45d32a122320174f3aa4f71b
Rainer Kildal <rainer.kildal@opscomsystems.com>
2023-01-06 09:45:41 +0100

    Clipboard and autocmds

--- lua/xerinox/set.lua
+++ lua/xerinox/set.lua
@@ -14,9 +14,10 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
 vim.opt.undofile = true
 
+vim.o.clipboard = 'unnamedplus'
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
