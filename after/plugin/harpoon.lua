ae0bbc24d8e180711e769ee9388e3bc0539793e5
Rainer Kildal <rainer.kildal@opscomsystems.com>
2023-01-09 13:55:35 +0100

    Fix some keybinds

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -10,13 +10,14 @@ end)
 vim.keymap.set("n", "<leader>da", mark.rm_file)
 vim.keymap.set("n", "<leader>ca", mark.clear_all)
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-l>", function() ui.nav_file(3) end)
+
 
 require("harpoon").setup({
 	menu = {
 		width = math.floor(vim.api.nvim_win_get_width(0) / 1.25),
 	}
 })
+
