839e196aac3fe5b9fc6007fc49c3c41ed90558e2
xerinox <rkildal@gmail.com>
2024-02-02 18:20:02 +0100

    Add diff keybinds for fugitive

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -2,6 +2,8 @@ return {
     "tpope/vim-fugitive",
     lazy = false,
     keys = {
-        { "<leader>gs", "<cmd>Git<CR>", desc = "Git Status" }
+        { "<leader>gs", "<cmd>Git<CR>", desc = "Git Status" },
+        { "<leader>gf", "<cmd>diffget //2<CR>", desc = "Git accept left change"},
+        { "<leader>gj", "<cmd>diffget //3<CR>", desc = "Git accept right change"}
     },
 }
